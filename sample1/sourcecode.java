db3e36a7cd81504328eae46a4e06f2dba146cf7a
“Prathap <prathapbaskaran12121996@gmail.com>
2022-11-19 09:16:08 +0000

    Seven

--- sample1/sourcecode.java
+++ sample1/sourcecode.java
@@ -4,7 +4,7 @@ import java.io.printer;
 import java.io.collection; 
 import java.io.scanner;
 import java.io.outputprinter;
-import java.io.testingi3;
+import java.io.PrathapBaskaran;
 public class SpiralPatternExample2
 {  
 //function to print the spiral pattern  
